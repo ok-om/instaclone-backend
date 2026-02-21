cb7eff66a4ab386050f37d67eb123e00f65bd538
ok-om <ok8023361@gmail.com>
2026-02-21 21:37:54 +0530

    applied cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cookieparser = require("cookie-parser");
-
+const cors = require("cors")
 const app = express();
 
 app.use(express.json());
-
+app.use(cors({
+    credentials:true,
+    origin:"http://localhost:5173"
+}))
 const authrouter = require("./routes/auth.routes");
 const postrouter = require("./routes/post.routes");
 const userrouter = require("./routes/user.routes");
