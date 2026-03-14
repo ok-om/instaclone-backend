3f12082329c064e895acbd8fdcd41ba436ab80d3
ok-om <ok8023361@gmail.com>
2026-03-14 17:54:58 +0530

    ready to deploy

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cookieparser = require("cookie-parser");
 const cors = require("cors")
 const app = express();
+const path = require("path");
 
 app.use(express.json());
 app.use(cors({
     credentials:true,
     origin:"http://localhost:5173",
 }))
+app.use(express.static(path.join(__dirname,"../Public")));
 const authrouter = require("./routes/auth.routes");
 const postrouter = require("./routes/post.routes");
 const userrouter = require("./routes/user.routes");
@@ -17,4 +19,9 @@ app.use(cookieparser());
 app.use("/api/post",postrouter)
 app.use("/api/auth", authrouter);
 app.use("/api/user",userrouter)
+
+app.get(/.*/,(req,res)=>{
+   res.sendFile(path.join(__dirname,"../Public","index.html"))
+})
+
 module.exports = app;
